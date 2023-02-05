57de9c0e57fdc6c4dfc9ccb4206ad91da241a0bb
collins <collinsolayemi@gmail.com>
2023-02-04 18:15:00 -0800

    testing message-bird

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const { engine } = require("express-handlebars");
+const messageBird = require("messagebird").initClient(
+  "EVCWzzfc64arpL2vgxRAr2prg"
+);
+const bodyParser = require("body-parser");
+const app = express();
+
+app.engine("handlebars", engine({ defaultLayout: "main" }));
+
+app.set("view engine", "handlebars");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("step1");
+});
+
+app.post("/step2", (req, res) => {
+  let number = req.body.number;
+
+  messageBird.verify.create(
+    number,
+    {
+      template: "Your Verification code is %token",
+    },
+    function (err, response) {
+      if (err) {
+        console.log(err);
+        res.render("step1", {
+          error: err.errors[0].description,
+        });
+      } else {
+        console.log(response);
+        res.render("step2", {
+          id: response.id,
+        });
+      }
+    }
+  );
+});
+
+app.post("/step3", (req, res) => {
+  const id = req.body.id;
+  const token = req.body.token;
+
+  messageBird.verify.verify(id, token, (err, response) => {
+    if (err) {
+      res.render("step2", {
+        error: err.errors[0].description,
+        id: id,
+      });
+    } else {
+      res.render("step3");
+    }
+  });
+});
+
+const port = 5555;
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
